60cf693ed19c5061b614b1da9bb8a5b6529e239c
Florent Aide <florent.aide@gmail.com>
2008-09-27 16:34:13 +0200

    Fixed the namespace

--- src/afpy/barcamp/tests.py
+++ src/afpy/barcamp/tests.py
@@ -1,12 +1,12 @@
 import os.path
 import z3c.testsetup
-import afpybarcamp
+import afpy.barcamp
 from zope.app.testing.functional import ZCMLLayer
 
 
 ftesting_zcml = os.path.join(
-    os.path.dirname(afpybarcamp.__file__), 'ftesting.zcml')
+    os.path.dirname(afpy.barcamp.__file__), 'ftesting.zcml')
 FunctionalLayer = ZCMLLayer(ftesting_zcml, __name__, 'FunctionalLayer',
                             allow_teardown=True)
 
-test_suite = z3c.testsetup.register_all_tests('afpybarcamp')
+test_suite = z3c.testsetup.register_all_tests('afpy.barcamp')
