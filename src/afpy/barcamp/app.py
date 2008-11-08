e47322b67274483f0aa95d3675ecd19c133215ef
Olivier Grisel <olivier.grisel@ensta.org>
2008-11-08 21:48:52 +0100

    embryonic work on style / templating

--- src/afpy/barcamp/app.py
+++ src/afpy/barcamp/app.py
@@ -28,6 +28,11 @@ class AddEvent(formlib.AddForm):
         self.context[name] = obj
         self.redirect(self.url('index'))
 
-class BarcampMacros(grok.View):
-    """The view providing the global macros"""
+class BarcampToplevelMacros(grok.View):
+    """The view providing the global macros for view upper than event level"""
     grok.context(Interface)
+
+class BarcampMacros(grok.View):
+    """The view providing the global macros for event under the event level"""
+    grok.context(Interface)
+
